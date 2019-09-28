3b57f1c341e13976e2a5c9326cbaaa46e23621b9
Mohammed Riaz Khan <riaz_khan96@outlook.com>
2019-09-28 20:54:50 +0200

    Updated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,13 @@ import {MatSliderModule} from '@angular/material';
 import { NavComponent } from './components/ui-components/site-components/nav/nav.component';
 import { FooterComponent } from './components/ui-components/site-components/footer/footer.component';
 import { CreateVisitComponent } from './components/ui-components/hospital/visit/create-visit/create-visit.component';
-import { CreateAppointmentComponent } from './components/ui-components/hospital/appointment/create-appointment/create-appointment.component';
 import { CreateDoctorComponent } from './components/ui-components/hospital/employee/doctor/create-doctor/create-doctor.component';
 import { CreatePatientComponent } from './components/ui-components/hospital/patient/create-patient/create-patient.component';
 import { PatientListComponent } from './components/ui-components/hospital/patient/patient-list/patient-list.component';
 import { EditPatientComponent } from './components/ui-components/hospital/patient/edit-patient/edit-patient.component';
 import { VisitListComponent } from './components/ui-components/hospital/visit/visit-list/visit-list.component';
-import { AppointmentListComponent } from './components/ui-components/hospital/appointment/appointment-list/appointment-list.component';
 import { DoctorListComponent } from './components/ui-components/hospital/employee/doctor/doctor-list/doctor-list.component';
-import { ReportComponent } from './components/ui-components/hospital/patient/report/report.component';
+
 
 @NgModule({
   declarations: [
@@ -24,15 +22,12 @@ import { ReportComponent } from './components/ui-components/hospital/patient/rep
     NavComponent,
     FooterComponent,
     CreateVisitComponent,
-    CreateAppointmentComponent,
     CreateDoctorComponent,
     CreatePatientComponent,
     PatientListComponent,
     EditPatientComponent,
     VisitListComponent,
-    AppointmentListComponent,
-    DoctorListComponent,
-    ReportComponent
+    DoctorListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/domain/appointment/appointment.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Appointment } from './appointment';
-
-describe('Appointment', () => {
-  it('should create an instance', () => {
-    expect(new Appointment()).toBeTruthy();
-  });
-});
