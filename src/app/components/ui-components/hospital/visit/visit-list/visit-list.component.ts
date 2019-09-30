1825c33cb1435fe85441ee52c516e9c5a1beab4b
Mohammed Riaz Khan <riaz_khan96@outlook.com>
2019-09-30 21:12:32 +0200

    Create Visit Component

--- src/app/components/ui-components/hospital/visit/visit-list/visit-list.component.ts
+++ src/app/components/ui-components/hospital/visit/visit-list/visit-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { VisitService } from 'src/app/services/visit/visit.service';
 import { Router } from '@angular/router';
 import { Visit } from 'src/app/domain/visit/visit';
+import { InPatient } from 'src/app/domain/patient/in-patient';
+import { PatientService } from 'src/app/services/patient/patient.service';
 
 @Component({
   selector: 'app-visit-list',
@@ -10,22 +12,58 @@ import { Visit } from 'src/app/domain/visit/visit';
 })
 export class VisitListComponent implements OnInit {
 
-  private visits: Visit[];
+  private visits: Visit[] = [];
+  private patients: InPatient[] = [];
+  private matchedPatients: InPatient[] = [];
 
   private visit:Visit;
   id:string;
 
-  constructor(private visitService:VisitService, private router:Router) { }
+  constructor(private visitService:VisitService, private patientService:PatientService, private router:Router) { }
 
   ngOnInit() {
     this.getVisits();
+    this.getPatients();
     this.setActive();
+    this.getAllPatientsLinkedWithVisit();
+
+    
+  }
+
+  getPatients(){
+    this.patientService.getAll().subscribe(data => {
+      this.patients = data;
+    })
   }
 
   getVisits(){
     this.visitService.getAll().subscribe(data => {
       this.visits = data;
     });
+  }
+
+  getAllPatientsLinkedWithVisit(){
+
+    this.visitService.getAll().subscribe(visits =>{
+
+      this.visits = visits;
+
+      this.patientService.getAll().subscribe(patients =>{
+
+          this.patients = patients;
+
+          for(var i = 0; i < visits.length; i++){
+             
+            for(var j = 0; j < patients.length; j++){
+              if(visits[i].patientId == patients[j].patientId){
+                visits[i].patient = patients[j];
+              }
+            }
+          }
+      });
+
+    });
+
   }
 
   deletePatient(id:string) {
