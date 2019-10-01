a848dddef18220cd81a7b030c66362713d1093b9
Mohammed Riaz Khan <riaz_khan96@outlook.com>
2019-10-01 11:24:40 +0200

    Changes to create visit view

--- src/app/components/ui-components/hospital/employee/doctor/create-doctor/create-doctor.component.ts
+++ src/app/components/ui-components/hospital/employee/doctor/create-doctor/create-doctor.component.ts
@@ -14,6 +14,40 @@ export class CreateDoctorComponent implements OnInit {
   doctor: Doctor = new Doctor();
   submitted = false;
 
+  days: String[] = [
+    '1', '2', '3', '4', '5',
+    '6', '7', '8', '9', '10',
+    '11', '12', '13', '14', '15',
+    '16', '17', '18', '19', '20',
+    '21', '22', '23', '24', '25',
+    '26', '27', '28', '29', '30'
+  ];
+
+  months: String[] = [
+    'Jan',
+    'Feb', 'Mar', 
+    'Apr',
+    'May',
+    'Jun', 'Jul', 
+    'Aug',
+    'Sep',
+    'Oct', 'Nov', 
+    'Dec'
+  ];
+
+  years: String[] = [
+    '1996', '1997', '1998',
+    '1999', '2000', '2001', '2002', '2003',
+    '2004', '2005', '2006', '2007', '2008',
+    '2009', '2010', '2011', '2012', '2013',
+    '2014', '2015', '2016', '2017', '2018',
+    '2019' 
+  ];
+
+  day:string;
+  month:string
+  year:string;
+
   constructor(private doctorService:DoctorService, private router:Router) { }
 
   ngOnInit() {
@@ -29,8 +63,9 @@ export class CreateDoctorComponent implements OnInit {
   }
 
   save(){
-
     this.doctor.employeeId = this.genId().toString();
+    this.doctor.employmentDate = this.getFullDate();
+    console.log(this.getFullDate());
     this.doctorService.createDoctor(this.doctor).subscribe(data => console.log(data), error => console.log(error));
     this.doctor = new Doctor();
   }
@@ -44,5 +79,38 @@ export class CreateDoctorComponent implements OnInit {
   cancel(){
     this.router.navigate(['/doctors']);
   }
+
+  getDay(value:string){
+
+    if(value != '-1'){
+      console.log(value);
+      this.day = value;
+    }
+
+  }
+
+  getMonth(value:string){
+
+    if(value != '-1'){
+      console.log(value);
+      this.month = value;
+    }
+
+  }
+
+  getYear(value:string){
+
+    if(value != '-1'){
+      console.log(value);
+      this.year = value;
+    }
+
+  }
+
+  getFullDate() : string{
+    return this.day + ' ' + this.month + ' ' + this.year;
+  }
+
+ 
 
 }
